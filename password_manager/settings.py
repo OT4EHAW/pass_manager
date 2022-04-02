46e5c8befddbbf1c040cbc0b885f31dd8fb82728
OT4EHAW <78928902+OT4EHAW@users.noreply.github.com>
2022-04-02 20:45:06 +0300

    Add files via upload

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(d-4slw#zqzfy8tcx_%*^@*u@lgrm)sb!vp&x+4#^9t^0m%a4t'
+SECRET_KEY = '1543MH2803221726'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
